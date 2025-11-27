2070f56f96d85097b316a3e0a1ee6a4cac6d9246
rojasday2323-dot <rojasday2323@gmail.com>
2025-11-27 15:29:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Proyecto Avance funcionando!");
+const buttons = document.querySelectorAll('.menu button');
+const panels = document.querySelectorAll('.panel');
+
+buttons.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    buttons.forEach(b => b.classList.remove('active'));
+    panels.forEach(p => p.classList.remove('visible'));
+
+    btn.classList.add('active');
+    panels[index].classList.add('visible');
+  });
+});
